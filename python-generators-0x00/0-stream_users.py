615acd3f3123b91ce96529c07c10c11a5bc6a731
diaa mohammad <diaamohammad078@gmail.com>
2025-09-14 00:12:27 +0300

    Add 0-stream_users file

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,18 @@
+
+
+def stream_users(connection):
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    for row in cursor:
+        yield row
+
+    cursor.close()
+
+
+
+
+    
+
+    
+
