4e000a7b134a445023155242b8ee64fd928aaa07
diaa mohammad <diaamohammad078@gmail.com>
2025-10-19 19:33:36 +0300

    add messaging app

--- Django-signals_orm-0x04/messaging_app/messaging/models.py
+++ Django-signals_orm-0x04/messaging_app/messaging/models.py
@@ -5,12 +5,18 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')  # ✅ added
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False)  
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.sender} → {self.receiver}: {self.content[:30]}"
+
+    def get_thread(self):
+        """Recursively get all replies"""
+        replies = self.replies.all().select_related('sender', 'receiver').prefetch_related('replies')
+        return replies
 
 
 class Notification(models.Model):
